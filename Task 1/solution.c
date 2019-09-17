017615b0ee8318d289a05979034be14fae9bfdc8
HIKAMURACHI <HIKAMURACHI@github.com>
2019-09-17 18:55:40 +0400

    Delete solution.c

--- Task 1/solution.c
+++
@@ -1,8 +0,0 @@
-#include <string.h>
-
-int stringStat(const char *string, size_t multiplier, int *count);
-
-int stringStat(const char *string, size_t multiplier, int *count) {
-    *count += 1;
-    return strlen(string) * multiplier;
-}
